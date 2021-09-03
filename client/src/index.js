0f150580adcc9dc5878869f0d459f419300c6797
lakshmi123-g <81859808+lakshmi123-g@users.noreply.github.com>
2021-09-03 12:35:23 +0530

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      < Route exact path="/page/:pageNumber" component={App} />
+      <Route exact path="/" component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
